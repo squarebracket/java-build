cd1772c580fed88ea90568e5262994c72ab433b8
Chuck Wilson <chuck.wilson+github@gmail.com>
2019-09-25 16:55:36 -0400

    test

---
+++ .github/actions/create-artifactory-token/lib/main.js
@@ -0,0 +1,80 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const http = __importStar(require("http"));
+const querystring_1 = require("querystring");
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const adminUsername = core.getInput('admin-username', { required: true });
+        const adminPassword = core.getInput('admin-password', { required: true });
+        const tokenUsername = core.getInput('token-username', { required: true });
+        const url = core.getInput('url', { required: true });
+        const promise = new Promise((resolve, reject) => {
+            const postData = querystring_1.stringify({
+                'username': tokenUsername
+            });
+            const options = {
+                auth: `${adminUsername}:${adminPassword}`,
+                method: 'POST',
+            };
+            const req = http.request(`${url}/api/security/token`, options, res => {
+                const { statusCode } = res;
+                const contentType = res.headers['content-type'];
+                let error;
+                if (statusCode !== 200) {
+                    error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+                }
+                else if (!/^application\/json/.test(contentType)) {
+                    error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+                }
+                if (error) {
+                    core.error(error.message);
+                    core.setFailed(error.message);
+                    // Consume response data to free up memory
+                    res.resume();
+                    reject();
+                    return;
+                }
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', (chunk) => { rawData += chunk; });
+                res.on('end', () => {
+                    try {
+                        const parsedData = JSON.parse(rawData);
+                        core.setOutput('artifactory-token', parsedData.access_token);
+                        resolve();
+                    }
+                    catch (e) {
+                        core.error(e.message);
+                        reject();
+                    }
+                });
+            }).on('error', (e) => {
+                core.error(`Got error: ${e.message}`);
+                reject();
+            });
+            req.write(postData);
+            req.end();
+        });
+        yield promise;
+    });
+}
+run();
---
+++ .github/actions/create-artifactory-token/src/main.ts
@@ -0,0 +1,64 @@
+import * as core from '@actions/core';
+import * as http from 'http';
+import { stringify } from 'querystring';
+
+async function run() {
+    const adminUsername = core.getInput('admin-username', { required: true });
+    const adminPassword = core.getInput('admin-password', { required: true });
+    const tokenUsername = core.getInput('token-username', { required: true });
+    const url = core.getInput('url', { required: true });
+
+    const promise = new Promise((resolve, reject) => {
+        const postData = stringify({
+            'username': tokenUsername
+        });
+        const options = {
+            auth: `${adminUsername}:${adminPassword}`,
+            method: 'POST',
+        };
+        const req = http.request(`${url}/api/security/token`, options, res => {
+            const { statusCode } = res;
+            const contentType: any = res.headers['content-type'];
+
+            let error;
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                    `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                    `Expected application/json but received ${contentType}`);
+            }
+            if (error) {
+                core.error(error.message);
+                core.setFailed(error.message);
+                // Consume response data to free up memory
+                res.resume();
+                reject();
+                return;
+            }
+
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    core.setOutput('artifactory-token', parsedData.access_token);
+                    resolve();
+                } catch (e) {
+                    core.error(e.message);
+                    reject();
+                }
+            });
+        }).on('error', (e) => {
+            core.error(`Got error: ${e.message}`);
+            reject();
+        });
+        req.write(postData);
+        req.end();
+    });
+
+    await promise;
+}
+
+run();
